fb231b7c71446912a2933a2075dc0891b27e945e
khalil benchekroun <benchekrounkhalil10@gmail.com>
2026-04-11 14:52:07 +0000

    sauvegarde

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import QRCodePage from "./pages/QRCode";
 import Services from "./pages/Services";
 import Finance from "./pages/Finance";
 import Support from "./pages/Support";
+import Tutorial from "./pages/Tutorial";
 import GlobalSearch from "./components/GlobalSearch";
 import Livraisons from "./pages/Livraisons";
 import Avis from "./pages/Avis";
@@ -348,6 +349,14 @@ export default function App() {
                   <AppLayout>
                     <Calendrier />
                   </AppLayout>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tutorial"
+              element={
+                <PrivateRoute>
+                  <Tutorial />
                 </PrivateRoute>
               }
             />
